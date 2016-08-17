feb5cf2f0d721389f23777314490c904f01784cc
Tim Preece <tdpreece@gmail.com>
2016-08-17 22:36:35 +0100

    Add a form without CSRF tokens.

--- web_app_under_test/mysite/app/urls.py
+++ web_app_under_test/mysite/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^contact-form$', views.ContactView.as_view(), name='contact-form'),
+    url(r'^thanks$', views.thanks, name='thanks'),
 ]
--- web_app_under_test/mysite/app/views.py
+++ web_app_under_test/mysite/app/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.edit import FormView
+
+from .forms import ContactForm
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'app/index.html')
+
+
+def thanks(request):
+    return HttpResponse("Thanks!")
+
+
+class ContactView(FormView):
+    template_name = 'app/contact.html'
+    form_class = ContactForm
+    success_url = 'thanks'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super(ContactView, self).form_valid(form)

d4852ec258db653787887ef3c7aea88043dfee12
Tim Preece <tdpreece@gmail.com>
2016-09-10 16:03:52 +0100

    Move url definition into app.

--- web_app_under_test/mysite/app/urls.py
+++ web_app_under_test/mysite/app/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', login, {'template_name': 'app/login.html'}, name='login'),
     url(r'^contact-form$', login_required(views.ContactView.as_view()), name='contact-form'),
     url(r'^thanks$', views.thanks, name='thanks'),
 ]
--- web_app_under_test/mysite/mysite/urls.py
+++ web_app_under_test/mysite/mysite/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login
 
 urlpatterns = [
-    url(r'^app/', include('app.urls')),
+    url(r'', include('app.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', login, {'template_name': 'app/login.html'}, name='login'),
 ]
